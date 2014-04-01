2312274ddfb63968d1e9bafde72afa4345674764
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-03-31 22:30:28 -0300

    Auth wrappers

---
+++ src/PolypApps/Bundle/BrainBundle/Resources/Service/MysqlAuthWrapper.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace PolypApps\Bundle\AuthBundle\Service;
+
+/**
+ * Description of MysqlAuthWrapper
+ *
+ * @author rodrigo
+ */
+class MysqlAuthWrapper implements AuthWrapper {
+    
+    public function login($username, $password) {
+        
+    }
+
+    public function logout() {
+        
+    }
+
+}
