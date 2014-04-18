2943f6743ace8763ba4db59cfa3d78c4673ff8d6
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-04-18 12:19:32 -0300

    auth testing

--- src/PolypApps/Bundle/AuthBundle/Controller/DefaultController.php
+++ src/PolypApps/Bundle/AuthBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace PolypApps\Bundle\AuthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use\Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return new \Symfony\Component\HttpFoundation\JsonResponse("This is the Auth Module");
+        return new JsonResponse("This is the Auth Module");
+    }
+    
+    public function loginAction(Request $request) {
+        $headers = $request->headers->all();
+        $post = $request->request;
+        $username = $post->get("username");
+        
+        return new JsonResponse(json_encode($headers));
     }
 }
